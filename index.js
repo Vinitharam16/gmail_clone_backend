7c51ec2dab0e30c66040de6bcab7d3809d096dfb
Vinitharam16 <vinithabegp@gmail.com>
2024-06-01 13:58:35 +0530

    third commit

--- index.js
+++ index.js
@@ -11,24 +11,8 @@ HTTP_SERVER.use(express.urlencoded({ extended: true }));
 HTTP_SERVER.use(express.json({ extended: true }))
 
 
-var whitelist = [
-  "http://127.0.0.1:5500",
-  undefined,
-  "https://gmail-clone-backend-o2y5.onrender.com/",
-
-];
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-};
-
 //enabling cors
-HTTP_SERVER.use(CORS(corsOptions));
+HTTP_SERVER.use(CORS());
 
 //configuring dotenv package
 require("dotenv").config();
@@ -40,7 +24,7 @@ connectToDatabase();
 // Defining A Port and listening to port with express server
 const PORT = process.env.DEV_SERVER_PORT;
 HTTP_SERVER.listen(PORT, process.env.NODE_HOSTNAME, () => {
-    console.log("Server started successfully" + PORT);
+    console.log(`Server started successfully ${PORT}`);
 });
 
 //enabling routes
