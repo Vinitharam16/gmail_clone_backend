669df2e93b93917d92a9ad3dbda97004fb3d9cb9
Vinitharam16 <vinithabegp@gmail.com>
2024-06-09 18:35:24 +0530

    fifth commit

--- routes/route.js
+++ routes/route.js
@@ -4,22 +4,22 @@ const { saveSentEmails, getEmails, moveEmailsToBin, getsendMails, toggleStarredE
 const routes = require('express').Router();
 
 //SAVED ALL EMAILS
-routes.get('/email', TokenShield, GET_ALL_MAILS)
+routes.get('/email',  GET_ALL_MAILS)
 
 // sent and save mails using post method
-routes.post('/save', TokenShield, saveSentEmails);
+routes.post('/save',  saveSentEmails);
 // save draft mails using post method
-routes.post('/save-draft', TokenShield, saveSentEmails);
+routes.post('/save-draft',  saveSentEmails);
 // get sent mails using get method
-routes.get('/emails/sent', TokenShield, getsendMails);
+routes.get('/emails/sent',  getsendMails);
 // get mails from bin, starred and allmails using get method
-routes.get('/emails/:type', TokenShield, getEmails);
+routes.get('/emails/:type',  getEmails);
 // move emails to bin using post method
-routes.post('/bin', TokenShield, moveEmailsToBin);
+routes.post('/bin',  moveEmailsToBin);
 // starred mails using post method
-routes.post('/starred', TokenShield, toggleStarredEmails);
+routes.post('/starred',  toggleStarredEmails);
 // delete mails using delete method
-routes.delete('/delete', TokenShield, deleteEmails);
+routes.delete('/delete',  deleteEmails);
 
 
 module.exports = routes;
